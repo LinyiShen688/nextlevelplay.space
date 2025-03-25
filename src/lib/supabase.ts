0507f42991b69d6cf2d43c7a7fa74b6227254451
linyi01code <linyi01code@foxmail.com>
2025-03-26 00:06:57 +0800

    game

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://qgteqonyzheydvhupdet.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFndGVxb255emhleWR2aHVwZGV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NjE2NzMsImV4cCI6MjA1ODIzNzY3M30.4blyu56CYjET0vWxiB9xeHA0g7arPtoR3YuvOJjuP6M';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
